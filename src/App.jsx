12b81ec2f8c3264baed9a8e80ad2c34b7e44c6a8
harshit24saxena <125709492+harshit24saxena@users.noreply.github.com>
2025-06-28 05:30:19 +0530

    Add src files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import AboutMe from "./comonents/AboutMe"
+import Contact from "./comonents/Contact"
+import Header from "./comonents/Header"
+import Navbar from "./comonents/Navbar"
+import Project from "./comonents/Project"
+import Skills from "./comonents/Skills"
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <AboutMe />
+    <Skills />
+    <Project />
+    <Contact />
+    </>
+  )
+}
+
+export default App

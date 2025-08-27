17d7b7faaa2623b3a7843493c3bbdf67b5b6902e
Harshit saxena <saxena24harshit@gmail.com>
2025-08-27 14:23:25 +0530

    correct image src url

--- src/utilities/SkillCard.jsx
+++ src/utilities/SkillCard.jsx
@@ -1,50 +1,50 @@
 const SkillList = [
   {
-    src: "./src/assests/rmbg/html-removebg-preview.png",
+    src: "/src/assests/rmbg/html-removebg-preview.png",
     name: "HTML",
   },
   {
-    src: "./src/assests/rmbg/css-removebg-preview.png",
+    src: "/src/assests/rmbg/css-removebg-preview.png",
     name: "css",
   },
   {
-       src: "./src/assests/rmbg/js-removebg-preview.png",
+       src: "/src/assests/rmbg/js-removebg-preview.png",
     name: "javascript",
   },
   {
-    src: "./src/assests/rmbg/node-removebg-preview.png",
+    src: "/src/assests/rmbg/node-removebg-preview.png",
     name: "node js",
   },
   {
-    src: "./src/assests/rmbg/express-removebg-preview.png",
+    src: "/src/assests/rmbg/express-removebg-preview.png",
     name: "express",
   },
   {
-      src: "./src/assests/rmbg/react-removebg-preview.png",
+      src: ./src/assests/rmbg/react-removebg-preview.png",
     name: "react",
   },
   {
-    src: "./src/assests/rmbg/tailwind-removebg-preview.png",
+    src: "/src/assests/rmbg/tailwind-removebg-preview.png",
     name: "tailwind",
   },
   {
-    src: "./src/assests/rmbg/git-removebg-preview.png",
+    src: "/src/assests/rmbg/git-removebg-preview.png",
     name: "git",
   },
   {
-    src: "./src/assests/rmbg/canva-removebg-preview.png",
+    src: "/src/assests/rmbg/canva-removebg-preview.png",
     name: "canva",
   },
   {
-    src: "./src/assests/rmbg/SQL-removebg-preview.png",
+    src: "/src/assests/rmbg/SQL-removebg-preview.png",
     name: "SQL",
   },
   {
-    src: "./src/assests/rmbg/mongoDB-removebg-preview.png",
+    src: "/src/assests/rmbg/mongoDB-removebg-preview.png",
     name: "MongoDB",
   },
   {
-    src: "./src/assests/rmbg/docker-removebg-preview.png",
+    src: "/src/assests/rmbg/docker-removebg-preview.png",
     name: "Docker",
   },
 ];
